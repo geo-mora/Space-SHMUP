4724b58fc4665a99cda99031c9a0d172711f6756
geo-mora <geovannym474@gmail.com>
2024-04-18 16:37:24 -0400

    Version 2.0 Class 4-18

--- Space SHMUP/Assets/_Scripts/BoundsCheck.cs
+++ Space SHMUP/Assets/_Scripts/BoundsCheck.cs
@@ -11,7 +11,10 @@ using UnityEngine;
 public class BoundsCheck : MonoBehaviour { // a
 [Header("Set in Inspector")]
 public float radius = 1f;
+public bool keepOnScreen = true;
+
 [Header("Set Dynamically")]
+public bool isOnScreen = true;
 public float camWidth;
 public float camHeight;
 
@@ -21,25 +24,39 @@ camWidth = camHeight * Camera.main.aspect; // c
 }
 
 void LateUpdate () { // d
-Vector3 pos = transform.position;
-if (pos.x > camWidth - radius) {
-	pos.x = camWidth - radius;
-}
-if (pos.x < -camWidth + radius) {
-	pos.x = -camWidth + radius;
-}
-if (pos.y > camHeight - radius) {
-	pos.y = camHeight - radius;
-} if (pos.y < -camHeight + radius) {
-	pos.y = -camHeight + radius;
-}
+	Vector3 pos = transform.position;
+	isOnScreen = true;
 
-transform.position = pos;
+	if (pos.x > camWidth - radius) {
+		pos.x = camWidth - radius;
+		isOnScreen = false;
+	}
+
+	if (pos.x < -camWidth + radius) {
+		pos.x = -camWidth + radius;
+		isOnScreen = false;
+	}
+
+	if (pos.y > camHeight - radius) {
+		pos.y = camHeight - radius;
+		isOnScreen = false;
+	} 
+
+	if (pos.y < -camHeight + radius) {
+		pos.y = -camHeight + radius;
+		isOnScreen = false;
+	}
+
+	if (keepOnScreen && !isOnScreen){
+		transform.position = pos;
+		isOnScreen = true;
+	}
 }
 
 // Draw the bounds in the Scene pane using OnDrawGizmos()
 void OnDrawGizmos () { // e
-if (!Application.isPlaying) return;
+	if (!Application.isPlaying) return;
 	Vector3 boundSize = new Vector3(camWidth* 2, camHeight* 2, 0.1f);
 	Gizmos.DrawWireCube(Vector3.zero, boundSize);
-}}
+	}
+}
